c54d65beaf826a1f0288784b9b6df6982039f3e8
AlpacaMale <alpaca9100@gmail.com>
2025-08-26 08:19:52 +0900

    feat: 라우터 경로 고침, req.body와 req.cookie 받을 수 있게함

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import v1UserRouter from "./routers/v1/users";
 import v1PostRouter from "./routers/v1/posts";
 import v1CommentRouter from "./routers/v1/comments";
-import v1AuthRouter from "./routes/v1/auth";
+import v1AuthRouter from "./routers/v1/auth";
 
 const app = express();
 
 // middleware
 app.use(morgan("dev")); // logger
+app.use(express.json()); // req.body
+app.use(cookieParser()); // req.cookies
 
 // router
 app.use("/v1/auth", v1AuthRouter);
