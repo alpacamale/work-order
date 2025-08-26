313048264174b7cc18b1c54839452397098cee76
AlpacaMale <alpaca9100@gmail.com>
2025-08-26 16:08:58 +0900

    feat: 평문에서 멘션 추출

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import User from "../models/User.js";
+import Post from "../models/Post.js";
 const { Schema, model } = mongoose;
 
 const commentSchema = new Schema(
@@ -10,6 +12,45 @@ const commentSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// @를 이용한 멘션
+
+commentSchema.pre("save", async function (next) {
+  try {
+    // 1. 댓글이 달린 게시글 가져오기
+    const post = await Post.findById(this.post).select("mentions").lean();
+    if (!post) {
+      return next(new Error("Invalid post reference"));
+    }
+
+    // 2. 댓글 내용에서 @username 패턴 추출
+    const mentionRegex = /@([a-zA-Z0-9_.-]+)/g;
+    const usernames = [];
+    let match;
+
+    while ((match = mentionRegex.exec(this.content)) !== null) {
+      usernames.push(match[1]);
+    }
+
+    if (usernames.length === 0) {
+      this.mentions = [];
+      return next();
+    }
+
+    // 3. 해당 username 가진 유저들 찾기
+    const users = await User.find({ username: { $in: usernames } });
+
+    // 4. 글 mentions 배열에 포함된 유저만 필터링
+    const allowedUserIds = post.mentions.map((id) => id.toString());
+    this.mentions = users
+      .filter((u) => allowedUserIds.includes(u._id.toString()))
+      .map((u) => u._id);
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 const Comment = model("Comment", commentSchema);
 export default Comment;
