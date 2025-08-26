e94bad38d34d7bd1009768834441f29ac7fc009b
AlpacaMale <alpaca9100@gmail.com>
2025-08-26 16:06:03 +0900

    fix: RESTful API에 맞게 수정

--- src/controllers/v1/authController.js
+++ src/controllers/v1/authController.js
@@ -2,37 +2,6 @@ import User from "../../models/User";
 import bcrypt from "bcrypt"; // 비밀번호 해시용
 import jwt from "jsonwebtoken";
 
-// 회원가입
-export const register = async (req, res) => {
-  try {
-    const { name, position, username, password } = req.body;
-
-    // 중복 체크
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: "이미 존재하는 아이디입니다." });
-    }
-
-    // 비밀번호 해시
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = new User({
-      name,
-      position,
-      username,
-      password: hashedPassword,
-    });
-    await user.save();
-
-    res.status(201).json({
-      message: "회원가입 성공",
-      user: { id: user._id, username: user.username },
-    });
-  } catch (err) {
-    res.status(500).json({ message: "회원가입 실패", error: err.message });
-  }
-};
-
 // 로그인
 export const login = async (req, res) => {
   try {
@@ -44,9 +13,13 @@ export const login = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
+      return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
 
     // JWT 발급
+    if (!process.env.JWT_SECRET)
+      return res
+        .status(503)
+        .json({ message: "환경변수가 설정되지 않았습니다." });
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
@@ -62,15 +35,4 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
   res.clearCookie("token");
   res.json({ message: "로그아웃 성공" });
-};
-
-// 회원탈퇴
-export const deleteAccount = async (req, res) => {
-  try {
-    const userId = req.user.id; // 미들웨어에서 JWT 검증 후 넣어줌
-    await User.findByIdAndDelete(userId);
-    res.json({ message: "회원탈퇴 성공" });
-  } catch (err) {
-    res.status(500).json({ message: "회원탈퇴 실패", error: err.message });
-  }
 };
--- src/routers/v1/auth.js
+++ src/routers/v1/auth.js
@@ -1,24 +1,12 @@
 import express from "express";
-import {
-  register,
-  login,
-  logout,
-  deleteAccount,
-} from "../../controllers/v1/authController.js";
-import { authMiddleware } from "../../middleware/authMiddleware.js";
+import { login, logout } from "../../controllers/v1/authController.js";
 
 const router = express.Router();
-
-// POST/v1 /auth/register
-router.post("/register", register);
 
 // POST /v1/auth/login
 router.post("/login", login);
 
 // POST /v1/auth/logout
 router.post("/logout", logout);
-
-// DELETE /v1/auth/delete
-router.delete("/delete", authMiddleware, deleteAccount);
 
 export default router;
