b420dd70674fa79e0b5b2f3f0b6131bf8bc506a6
AlpacaMale <alpaca9100@gmail.com>
2025-08-26 16:11:12 +0900

    fix: RESTful API에 맞게 수정

--- src/routers/v1/posts.js
+++ src/routers/v1/posts.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import {
   getPosts,
   getPost,
@@ -6,17 +7,24 @@ import {
   updatePost,
   deletePost,
 } from "../../controllers/v1/postController";
+import {
+  getComments,
+  createComment,
+} from "../../controllers/v1/commentController";
 
 // mapping route to function
 const router = express.Router();
+
+router.route("/").get(getPosts).post(createPost);
 
 router.param("id", (req, res, next, id) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "Invalid ObjectId" });
   }
+  next();
 });
 
-router.route("/").get(getPosts).post(createPost);
+router.route("/:id/comments").get(getComments).post(createComment);
 router.route("/:id").get(getPost).put(updatePost).delete(deletePost);
 
 // export file for import in other files
