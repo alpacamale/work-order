8266003e98c215038c082c7166fedff9f8323d1b
AlpacaMale <alpaca9100@gmail.com>
2025-08-26 20:28:44 +0900

    feat: 채팅방, 메시지 라우터

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import v1UserRouter from "./routers/v1/users";
 import v1PostRouter from "./routers/v1/posts";
 import v1CommentRouter from "./routers/v1/comments";
 import v1AuthRouter from "./routers/v1/auth";
+import v1ChatRoomRouter from "./routers/v1/chatRoom";
+import v1MessageRouter from "./routers/v1/message";
 import { authMiddleware } from "./middleware/authMiddleware";
 
 const app = express();
@@ -22,5 +24,7 @@ app.use("/v1/users", v1UserRouter);
 app.use(authMiddleware);
 app.use("/v1/posts", v1PostRouter);
 app.use("/v1/comments", v1CommentRouter);
+app.use("/v1/chat-rooms", v1ChatRoomRouter);
+app.use("/v1/messages", v1MessageRouter);
 
 export default app;
